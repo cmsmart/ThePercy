446078034ad98e8400c9022a4590559c9a3e13fc
Hannah Bourke <hannah25@gmail.com>
2018-01-10 10:14:57 +1100

    Create timer component

---
+++ src/containers/Timer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+
+class Timer extends Component {
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Timer
