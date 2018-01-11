3b43ad041ab19b521f62b961e000b6e9bcb5a3f2
Cholena <cholena@gmail.com>
2018-01-11 14:04:13 +1100

    Hannah styling (#8)
    * Added css
    
    * Add sass and basic style
    
    * Add variables sub folder for scss
    
    * Add font as a variable

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import "../App.css";
 
 import { Dashboard } from '../components/Dashboard'
 import { Historical } from '../components/Historical'
