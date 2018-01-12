bc01a2f1fb9b692277b1dfbc9319f900fd1dfe58
GretchS <gretchen.scott@hotmail.com>
2018-01-13 10:41:16 +1100

    LineChart renders data correctly. Needs labels and legend still.

--- src/components/MusherLineChart.js
+++ src/components/MusherLineChart.js
@@ -36,16 +36,16 @@ class MusherLineChart extends Component {
       <LineChart width={300} height={300} margin={{top: 30, right: 30, left: 20, bottom: 5}}>
         <CartesianGrid strokeDasharray="3 3"/>
         <XAxis dataKey="time" type="time" domain={[0, 50]}/>
-        <YAxis  dataKey="dist"  type="number" allowDuplicatedCategory={false} domain={[0, 338]}/>
+        <YAxis  dataKey="dist"  type="number"  allowDuplicatedCategory={false} domain={[0, 338]}/>
         <Tooltip/>
         <Legend/>
         {series.map(s => (
-          <Line type="monotone" dataKey="time" data={s.data} name={s.year} key={s.year} stroke="#4e7590"  strokeWidth="3" />
+          <Line type="monotone" dataKey="dist" data={s.data} name={s.year} key={s.year} stroke="#4e7590"  strokeWidth="2" />
         ))}
-          <ReferenceLine y={80.4} stroke="red" label={{ position: "end", value: "Fortymile in", fill:"red" }}  />
-          <ReferenceLine y={159.87} stroke="red" label={{ position: "top", value: "Eagle", fill:"red" }} />
-          <ReferenceLine y={240.27} stroke="red" label={{ position: "top", value: "Fortymile out", fill:"red" }} />
-          <ReferenceLine y={338} stroke="red" label={{ position: "top", value: "Finish", fill:"red" }} />
+          <ReferenceLine y={80.4} stroke="red" strokeWidth="0.5" label={{ position: "end", value: "Fortymile in", fill:"red" }}  />
+          <ReferenceLine y={159.87} stroke="red" strokeWidth="0.5" label={{ position: "top", value: "Eagle", fill:"red" }} />
+          <ReferenceLine y={240.27} stroke="red" strokeWidth="0.5" label={{ position: "top", value: "Fortymile out", fill:"red" }} />
+          <ReferenceLine y={338} stroke="red" strokeWidth="0.5" label={{ position: "top", value: "Finish", fill:"red" }} />
       </LineChart>
       </ResponsiveContainer>
       </div>
