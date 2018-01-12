bf2778f184ed70b2d978832326cb25811d949cd0
GretchS <gretchen.scott@hotmail.com>
2018-01-13 10:14:39 +1100

    Changed axes of linechart to be correct but has a bug displaying data correctly

--- src/components/MusherLineChart.js
+++ src/components/MusherLineChart.js
@@ -31,21 +31,21 @@ const series = [
 class MusherLineChart extends Component {
 	render () {
   	return (
-      <div className="area-chart-wrapper" style={{ width: '100%', height: '400px' }}>
+      <div className="area-chart-wrapper" style={{ width: '50%', height: '400px' }}>
       <ResponsiveContainer>
       <LineChart width={300} height={300} margin={{top: 30, right: 30, left: 20, bottom: 5}}>
         <CartesianGrid strokeDasharray="3 3"/>
-        <XAxis dataKey="dist"  type="number" allowDuplicatedCategory={false} domain={[0, 338]}/>
-        <YAxis dataKey="time" type="time" domain={[0, 50]} />
+        <XAxis dataKey="time" type="time" domain={[0, 50]}/>
+        <YAxis  dataKey="dist"  type="number" allowDuplicatedCategory={false} domain={[0, 338]}/>
         <Tooltip/>
         <Legend/>
         {series.map(s => (
           <Line type="monotone" dataKey="time" data={s.data} name={s.year} key={s.year} stroke="#4e7590"  strokeWidth="3" />
         ))}
-          <ReferenceLine x={80.4} stroke="red" label={{ position: "top", value: "Fortymile in", fill:"red" }}  />
-          <ReferenceLine x={159.87} stroke="red" label={{ position: "top", value: "Eagle", fill:"red" }} />
-          <ReferenceLine x={240.27} stroke="red" label={{ position: "top", value: "Fortymile out", fill:"red" }} />
-          <ReferenceLine x={338} stroke="red" label={{ position: "top", value: "Finish", fill:"red" }} />
+          <ReferenceLine y={80.4} stroke="red" label={{ position: "end", value: "Fortymile in", fill:"red" }}  />
+          <ReferenceLine y={159.87} stroke="red" label={{ position: "top", value: "Eagle", fill:"red" }} />
+          <ReferenceLine y={240.27} stroke="red" label={{ position: "top", value: "Fortymile out", fill:"red" }} />
+          <ReferenceLine y={338} stroke="red" label={{ position: "top", value: "Finish", fill:"red" }} />
       </LineChart>
       </ResponsiveContainer>
       </div>
