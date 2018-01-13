64056d323dcf97dca341bf176322ae6a80ed90fe
Hannah Bourke <hannah25@gmail.com>
2018-01-13 23:09:59 +1100

    Update navbar to make compatible with merge

--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
 
-// const NavItem = (props) => {
-//   return (
-//     <li className={props.onClick()} >
-//       <a href={props.path}>
-//         <img className="nav-icons" src={require(`../assets/${props.image}`)} alt="icon"/>
-//         {props.title}
-//       </a>
-//     </li> 
-//   )
-// }
+const NavItem = (props) => {
+  return (
+    <li>
+      <a href={props.path}>
+        <img className="nav-icons" src={require(`../../assets/${props.image}`)} alt="icon"/>
+        {props.title}
+      </a>
+    </li> 
+  )
+}
 
 class NavBar extends Component{
 
@@ -17,12 +17,11 @@ class NavBar extends Component{
   return (
     <nav>
       <ul>
-        {/* <NavItem path="/tracker" image="map.svg" title="Race Map"/>
+        <NavItem path="/tracker" image="map.svg" title="Race Map"/>
         <NavItem path="/dashboard" image="dashboard.svg" title="Dashboard" />
         <NavItem path="/dashboard" image="dashboard_historical.svg" title="Past Races" />
         <NavItem path="/mushers" image="team.svg" title="Mushers" />
-        <NavItem path="/statistics" image="analytics.svg" title="Statistics" /> */}
-        <li>test</li>
+        <NavItem path="/statistics" image="analytics.svg" title="Statistics" />
       </ul>
     </nav>
   )
