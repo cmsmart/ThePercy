821c87ab9104c1d88512e2e139b999d37fbf6d4e
Cholena <cholena@gmail.com>
2018-01-13 16:11:27 +1100

    Add style changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ export const Header = (props) => {
         <div className="info">
           <img className="logo" src={require('../../assets/whitelogo.png')} alt="logo" width={150} height={113}/>
           <section>
-            <h1 className='site-title'>The Percy DeWolfe Memorial Mail Race</h1>
+            <h1 className='site-title'><a href='/'>The Percy DeWolfe Memorial Mail Race</a></h1>
             <p className='subtitle'>22 March 2018 10:00am   |   Dawson City, YT - Eagle, AK</p>
           </section>
           <aside>
