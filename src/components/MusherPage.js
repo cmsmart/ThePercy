6e73810c190975d8f7f1b6e400dc36f44f77dd4e
Hannah Bourke <hannah25@gmail.com>
2018-01-16 14:36:46 +1100

    Get musher id from path

--- src/components/MusherPage.js
+++ src/components/MusherPage.js
@@ -1,18 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react';
+import MusherHistoryChart from '../components/MusherRaceHistory';
+import MusherLineChart from './MusherLineChart';
+import { getMushers, getMusher } from '../api/mushers';
 
-import MusherHistoryChart from '../components/MusherRaceHistory'
-import MusherLineChart from './MusherLineChart'
+class MusherPage extends Component {
+  state = {
+    musher_id: this.props.match.params.id,
+    mushers: {},
+    musher: ''
+  };
 
-export const MusherPage = (props) => {
-  return (
-    <div className='musher-page'>
-      <h1>Mushers Name</h1>
+  componentDidMount() {
+    getMushers()
+    .then(res => {
+      this.setState({ mushers: res })
+    }).then(() => {
+      let filteredMushers = this.state.mushers.filter(musher => (
+        musher.musher_id === this.state.musher_id
+      )
+    )
+    console.log(filteredMushers)
+    this.setState({ musher: filteredMushers })
+    console.log('musher state', this.state.musher)
+    })
+  }
 
-      <MusherHistoryChart />
-      <MusherLineChart />
 
-    </div>
-  )
+  render() {
+    return (
+      <div className="musher-page">
+        <h1>{
+          this.state.musher[0] &&
+          this.state.musher[0].musher}</h1>
+        <MusherHistoryChart />
+        <MusherLineChart />
+      </div>
+    );
+  }
 }
 
 export default MusherPage
