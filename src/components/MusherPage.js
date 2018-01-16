319f9481752b75d8b2b634ecd30e72547baf229b
Hannah Bourke <hannah25@gmail.com>
2018-01-17 10:41:37 +1100

    Add line chart api call

--- src/components/MusherPage.js
+++ src/components/MusherPage.js
@@ -2,41 +2,63 @@ import React, { Component } from 'react';
 import MusherHistoryChart from '../components/MusherRaceHistory';
 import MusherLineChart from './MusherLineChart';
 import { getMushers, getMusher } from '../api/mushers';
+import ProfileCard from './ProfileCard';
+import MyLineChart from './LineChart';
 
 class MusherPage extends Component {
   state = {
     musher_id: this.props.match.params.id,
+    musher: null,
     mushers: {},
-    musher: ''
   };
 
   componentDidMount() {
-    getMushers()
+    getMusher(this.state.musher_id)
     .then(res => {
-      this.setState({ mushers: res })
-    }).then(() => {
-      let filteredMushers = this.state.mushers.filter(musher => (
-        musher.musher_id === this.state.musher_id
-      )
-    )
-    console.log(filteredMushers)
-    this.setState({ musher: filteredMushers })
-    console.log('musher state', this.state.musher)
+      this.setState({ musher: res[0] })
+      
     })
+    .then(() => {
+      console.log(this.state.musher)
+    })
+    // getMushers()
+    // .then(res => {
+    //   this.setState({ mushers: res })
+    // }).then(() => {
+    //   let filteredMushers = this.state.mushers.filter(musher => (
+    //     musher.musher_id === this.state.musher_id
+    //   )
+    // )
+    // console.log(filteredMushers)
+    // this.setState({ musher: filteredMushers })
+    // console.log('musher state', this.state.musher)
+    // })
+  }
+  getImageAddress(musher) {
+    const imageAddress = musher.profile_image.uri.split("//"); 
+    const image = imageAddress[1];
+    return image
   }
 
 
   render() {
+    const { musher, musher_id } = this.state;
+    
     return (
-      <div className="musher-page">
-        <h1>{
-          this.state.musher[0] &&
-          this.state.musher[0].musher}</h1>
+    <div className="musher-page">
+        { !!this.state.musher &&
+        <div>
+        <h1>{musher.musher}</h1>
+        <div className="field">
+          <ProfileCard src={ this.getImageAddress(musher) } />
+        </div>
+        </div>
+      }
+        <MyLineChart musher_id={musher_id} />
         <MusherHistoryChart />
-        <MusherLineChart />
       </div>
-    );
-  }
+)
+}
 }
 
 export default MusherPage
