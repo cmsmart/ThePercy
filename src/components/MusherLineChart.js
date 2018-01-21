db1cf05e47ffc81c35937daff41490226ce45a64
Hannah Bourke <hannah25@gmail.com>
2018-01-21 11:51:10 +1100

    Edit line chart legend

--- src/components/MusherLineChart.js
+++ src/components/MusherLineChart.js
@@ -30,7 +30,7 @@ const renderLegend = props => {
             return (
               <li
                 key={`item-${index}`}
-                style={{ backgroundColor: `${backgroundColor}` }}
+                style={{ color: `${backgroundColor}` }}
               >
                 {event.year}
               </li>
@@ -53,39 +53,23 @@ const EventColor = [
 ];
 
 const MusherLineChart = props => {
-  return (
-    <div className="line-chart-wrapper">
+  return <div className="line-chart-wrapper">
       <h3>Past Race Results</h3>
-      <ResponsiveContainer padding="1rem">
+      <ResponsiveContainer padding="1rem" style={{ height: "500px" }}>
         <LineChart margin={{ top: 40, right: 20, left: 30, bottom: 90 }}>
           <CartesianGrid strokeDasharray="3 3" horizontal={false} />
-          <XAxis
-            dataKey="time"
-            type="number"
-            domain={[0, 50]}
-            ticks={[10, 20, 30, 40, 50]}
-          >
+          <XAxis dataKey="time" type="number" domain={[0, 50]} ticks={[10, 20, 30, 40, 50]}>
             <Label offset={-25} position="insideBottom">
               Time
             </Label>
           </XAxis>
-          <YAxis
-            dataKey="distance"
-            type="number"
-            allowDuplicatedCategory={false}
-            domain={[0, 338]}
-          >
+          <YAxis dataKey="distance" type="number" allowDuplicatedCategory={false} domain={[0, 338]}>
             <Label angle={-90} offset={10} position="insideBottomLeft">
               Distance (kms)
             </Label>
           </YAxis>
           <Tooltip />
-          <Legend
-            layout="vertical"
-            verticalAlign="middle"
-            align="right"
-            content={renderLegend}
-          />
+          <Legend layout="vertical" verticalAlign="middle" align="right" content={renderLegend} />
           {props.data.map(s => (
             <Line
               {...props}
@@ -102,54 +86,13 @@ const MusherLineChart = props => {
               name={event.event_id}
             />
           ))}
-          <ReferenceLine
-            y={80.4}
-            stroke="#0C2639"
-            strokeWidth="2"
-            label={{
-              position: "top",
-              value: "Fortymile Inbound",
-              fontSize: "0.8em",
-              scaleToFit: true
-            }}
-          />
-          <ReferenceLine
-            y={159.87}
-            stroke="#0C2639"
-            label={{
-              position: "top",
-              value: "Eagle",
-              fontSize: "0.8em",
-              fill: "#0C2639",
-              scaleToFit: true
-            }}
-          />
-          <ReferenceLine
-            y={240.27}
-            stroke="#0C2639"
-            label={{
-              position: "top",
-              value: "Fortymile Outbound",
-              fontSize: "0.8em",
-              fill: "#0C2639",
-              scaleToFit: true
-            }}
-          />
-          <ReferenceLine
-            y={338}
-            stroke="#0C2639"
-            label={{
-              position: "top",
-              value: "Finish Dawson",
-              fontSize: "0.8em",
-              fill: "#0C2639",
-              scaleToFit: true
-            }}
-          />
+          <ReferenceLine y={80.4} stroke="#0C2639" strokeWidth="2" label={{ position: "top", value: "Fortymile Inbound", fontSize: "0.8em", scaleToFit: true }} />
+          <ReferenceLine y={159.87} stroke="#0C2639" label={{ position: "top", value: "Eagle", fontSize: "0.8em", fill: "#0C2639", scaleToFit: true }} />
+          <ReferenceLine y={240.27} stroke="#0C2639" label={{ position: "top", value: "Fortymile Outbound", fontSize: "0.8em", fill: "#0C2639", scaleToFit: true }} />
+          <ReferenceLine y={338} stroke="#0C2639" label={{ position: "top", value: "Finish Dawson", fontSize: "0.8em", fill: "#0C2639", scaleToFit: true }} />
         </LineChart>
       </ResponsiveContainer>
-    </div>
-  );
+    </div>;
 };
 
 export default MusherLineChart;
