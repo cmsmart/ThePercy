f0087af6337fe795b38f71e70ef30e6f8fa5d817
Hannah Bourke <hannah25@gmail.com>
2018-01-21 12:29:35 +1100

    update results

--- src/components/Results.js
+++ src/components/Results.js
@@ -4,10 +4,7 @@ import { Field } from "./Field";
 export const Results = props => {
   return (
     <div className="field">
-<<<<<<< HEAD
       <h2>Mushers (from 2012)</h2>
-=======
->>>>>>> line-chart-format
       <div className="myCards">
       {!!props.data &&
          (
