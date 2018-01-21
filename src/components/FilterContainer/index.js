f50bc137e602a6e7b273479beb3998688069f1e7
Carl Camit <carlcamit0@gmail.com>
2018-01-21 15:32:25 +1100

    removed redundant FilterContainer folder

--- src/components/FilterContainer/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const FilterContainer = (props) => { 
-    return (
-        <div className="searchFilter">
-            {props.children}
-        </div>
-    )   
-}
