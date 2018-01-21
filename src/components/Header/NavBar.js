e1d4f085beac0d7c1abe5a44b492b9cadd7e660b
Cholena <cholena@gmail.com>
2018-01-21 18:03:54 +1100

    Nav bar as well

--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -8,7 +8,7 @@ export const NavBar = (props) => {
   return <nav>
       <ul>
         <NavItem path="/tracker" viewBox="0 0 55.017 55.017" svg={mapsvg} title="Race Map" />
-        <NavItem path="/dashboard" svg={dashboardsvg} title="Current Race" />
+        <NavItem path="/currentrace" svg={dashboardsvg} title="Current Race" />
         <NavItem path="/pastraces" svg={dashboardsvg} title="Past Races" />
         <NavItem path="/mushers" svg={teamsvg} title="Mushers" />
         <NavItem path="/statistics" svg={analyticssvg} title="Statistics" />
