3473d8b5ef52065ca4b777eac5ca2228669eae0b
Hannah Bourke <hannah25@gmail.com>
2018-01-22 17:04:32 +1100

    Add john's api

---
+++ src/api/races.js
@@ -0,0 +1,9 @@
+import johnapi from "./init";
+
+export function getRaceDataByEvent(id) {
+  return johnapi.get(`./pos-by-event/${id}`);
+}
+
+export function getRaceDataByMusher(id) {
+  return johnapi.get(`./pos-by-musher/${id}`);
+}
