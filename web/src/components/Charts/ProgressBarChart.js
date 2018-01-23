e479882e67132bc5e81b94a218576529878292a3
GretchS <gretchen.scott@hotmail.com>
2018-01-24 09:36:54 +1100

    Custom tool tip editing

--- web/src/components/Charts/ProgressBarChart.js
+++ web/src/components/Charts/ProgressBarChart.js
@@ -158,93 +158,91 @@ const series = [
     
 
 
-    class CustomTooltip extends Component {
+class CustomTooltip extends Component {
   render() {
    const { active, mushers, payload, label } = this.props;
-     console.log(payload)
- 
- 
-      if (active) {
-        // const { payload, label, name } = this.props;
-        return (
-          <div className="custom-tooltip">
-     
-         
+     //console.log(payload)
+    if (active) {
+      return (
+        <div className="custom-tooltip">
           <p className="label">{`Musher id: ${payload[1].value} `}</p>  
-          <p className="label"> { ` Distance (km): ${payload[1].payload.distance}`}</p> 
-          <p className="label"> { ` Time (hrs): ${payload[0].payload.time} `}</p>  
-          </div>
-              
-            );
-          }
-        return null;
-        }
-      }
+          <p className="label"> { `Distance (km): ${payload[1].payload.distance}`}</p> 
+          <p className="label"> { `Time (hrs): ${payload[0].payload.time} `}</p>  
+        </div>
+      );
+    }
+    return null;
+  }
+}
 
+  const data = generateData(event_updates, "musher_id")
+  //console.log(data)
 
-      const data = generateData(event_updates, "musher_id")
-//console.log(data)
-
-const ProgressBarChart = (props) => {
+  const ProgressBarChart = (props) => {
     const payload = this.props;
     const dataseries = generateData(props.raceData, "musher_id")
-    //console.log(dataseries)
+      //console.log(dataseries)
 
-
-    const  experienceFilter = (mushers, pastmushers) => {
-      let experienceArray = []
+  const  experienceFilter = (mushers, pastmushers) => {
+    let experienceArray = []
       mushers.map((datum) => {
-          if (pastmushers.some((object) => (object.musher_id === datum.musher_id))) {
-              experienceArray = [ ...experienceArray, { name: datum.musher, experience: true } ]
-          } else {
-              experienceArray = [ ...experienceArray, { name: datum.musher, experience: false } ]
-          }
+        if (pastmushers.some((object) => (object.musher_id === datum.musher_id))) {
+          experienceArray = [ ...experienceArray, { name: datum.musher, experience: true } ]
+        } else {
+          experienceArray = [ ...experienceArray, { name: datum.musher, experience: false } ]
+        }
           return experienceArray
-      })
-      return experienceArray
-    }
+        })
+    return experienceArray
+  }
 
   return (
     <div className="area-chart-wrapper" style={{ width: '85%', height: "600px", backgroundColor: "#f8f8f8", border: "1px solid black", margin: "10px" }} display= "inline-block">
-    <h2>Musher Progress</h2>
-      <ResponsiveContainer>
-        <LineChart width={300} height={500} margin={{top: 50, right: 50, left: 50, bottom: 100}}>
-          <XAxis dataKey="distance" type="number" domain={[0, 320]} ticks={[80.4, 159.8, 239.2, 320]}>
-            <Label value="Distance (km)" offset={-15} position="insideBottom" />
-          </XAxis>
+      <h2>Musher Progress</h2>
+        <ResponsiveContainer>
+          <LineChart width={300} height={500} margin={{top: 50, right: 50, left: 50, bottom: 100}}>
 
-          <YAxis type="category" dataKey="musher_id" domain={[0 ]} >
-            <Label value="Musher id: " angle={-90} offset={-15} position="insideLeft" style={{ textAnchor: 'middle' }} />
-          </YAxis>
+            <XAxis dataKey="distance" type="number" domain={[0, 320]} ticks={[80.4, 159.8, 239.2, 320]}>
+              <Label value="Distance (km)" offset={-15} position="insideBottom" />
+            </XAxis>
 
-  <Tooltip content={<CustomTooltip />} {...props}/>
+            <YAxis type="category" dataKey="musher_id" domain={[0 ]} >
+              <Label value="Musher id: " angle={-90} offset={-15} position="insideLeft" style={{ textAnchor: 'middle' }} />
+            </YAxis>
+
+            <Tooltip content={<CustomTooltip />} {...props}/>
           
-        {dataseries.map(s => (
-          <Line dataKey="musher_id" data={s.data} 
-          name={s.musher_id} key={s.musher_id} strokeWidth="13" activeDot={{r: 8}}
-          type="monotone"  strokeDasharray="5 5"  dot={{strokeDasharray: 'none'}} />
-        
-         
-        ))}
- 
+          {dataseries.map(s => (
+            <Line dataKey="musher_id" 
+            data={s.data} 
+            name={s.musher_id} 
+            key={s.musher_id} 
+            strokeWidth="13" 
+            activeDot={{r: 8}}
+            type="monotone"  
+            strokeDasharray="5 5"  
+            dot={{strokeDasharray: 'none'}} />
+          ))}
+  
 
-        {ExperienceColour.map((experience) => {
-        <Line datakey="musher_id"
-         key={experience.musher_id}
-        stroke={experience.lineColor}
-        name={experience.experience}
-        strokeWidth='12'
-      />
+          {ExperienceColour.map((experience) => {
+            <Line datakey="musher_id"
+            key={experience.musher_id}
+            stroke={experience.lineColor}
+            name={experience.experience}
+            strokeWidth='12'/>
           })}
           
             <ReferenceLine x={80.4} stroke="#FA5252" label={{ position: "top", value: "Fortymile", fontSize: '0.8em',  fill: "#FA5252", scaleToFit: true }} />
             <ReferenceLine x={159.8} stroke="#FA5252" label={{ position: "top", value: "Eagle", fontSize: '0.8em', fill: "#FA5252", scaleToFit: true }} />
             <ReferenceLine x={239.2} stroke="#FA5252" label={{ position: "top", value: "Fortymile", fontSize: '0.8em', fill: "#FA5252", scaleToFit: true }} />
             <ReferenceLine x={320} stroke="#FA5252" label={{ position: "top", value: "Finish Dawson", fontSize: '0.8em', fill: "#FA5252", scaleToFit: true }} />
+
         </LineChart>
+
       </ResponsiveContainer>
     </div>
-    )}
+  )}
 
 
 export default ProgressBarChart
