1e570b730315e148489375a267045cabc35c6a4a
Cholena <cholena@gmail.com>
2018-03-21 07:53:56 +1100

    Change api address and no https

--- web/src/api/init.js
+++ web/src/api/init.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://thepercy.com/percy'
+  baseURL: 'https://thepercy.com/percy'
   // baseURL: 'http://percy.cholenasmart.com/percy'
 })
 
 export const johnapi = axios.create({
-  baseURL: "http://api.mammothgeo.com/api"
+  baseURL: "https://api.mammothgeo.com/api"
 });
 
