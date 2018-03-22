6b190dbf0f512070d10cee78100c139b30218532
Cholena <cholena@gmail.com>
2018-03-23 08:17:34 +1100

    Remove timer

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import TimerContainer from '../TimerContainer/index'
+// import TimerContainer from '../TimerContainer/index'
 import { NavBar } from './NavBar'
 
 export const Header = (props) => {
@@ -13,9 +13,9 @@ export const Header = (props) => {
                     <h1 className='site-title'><a href='/'>The Percy DeWolfe Memorial Mail Race</a></h1>
                     <p className='subtitle'>22 March 2018 10:00am   |   Dawson City, YT - Eagle, AK</p>
                 </section>
-                <aside className="timer-wrapper ">
+                {/* <aside className="timer-wrapper ">
                     <TimerContainer />
-                </aside>
+                </aside> */}
                 <aside className="social">
                     <img className="twitter" src={require('../../assets/twitter.svg')} alt="logo" width={30} height={24}/>
                     <img className="facebook" src={require('../../assets/facebook.svg')} alt="logo" width={30} height={30}/>
