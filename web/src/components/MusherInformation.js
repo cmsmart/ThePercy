1eff97cf3dcdca538dbf790cb6351779fcaf9873
Cholena <cholena@gmail.com>
2018-03-23 16:55:00 +1100

    Add personal data back with additional condition

--- web/src/components/MusherInformation.js
+++ web/src/components/MusherInformation.js
@@ -13,14 +13,16 @@ const getTopByKeyCount = (data, key, topLimit) => {
 }
 
 const generateInformation = (data, race) => {
-    let informationArray = []
-    let forRace = data.filter((datum) => (datum.race === race && datum.run_time.toLowerCase() !== 'unknown' && datum.run_time.toLowerCase() !== 'scratched' && datum.run_time !== ''))
-    let sortedStandingData = forRace.slice().sort(compareObjectValuesNumeric('standing'))[0]
-    let bestStanding = `${sortedStandingData.standing} in ${sortedStandingData.year}`
-    let sortedRunTimeData = forRace.sort(compareObjectValues('run_time'))[0]
-    let bestFinishTime = `${sortedRunTimeData.run_time} in ${sortedRunTimeData.year}`
-    let runCount = getTopByKeyCount(forRace, 'races', 1)[0].races
-    return informationArray = [ runCount, bestStanding, bestFinishTime ]
+    if (data.standing != '') {
+        let informationArray = []
+        let forRace = data.filter((datum) => (datum.race === race && datum.run_time.toLowerCase() !== 'unknown' && datum.run_time.toLowerCase() !== 'scratched' && datum.run_time !== ''))
+        let sortedStandingData = forRace.slice().sort(compareObjectValuesNumeric('standing'))[0]
+        let bestStanding = `${sortedStandingData.standing} in ${sortedStandingData.year}`
+        let sortedRunTimeData = forRace.sort(compareObjectValues('run_time'))[0]
+        let bestFinishTime = `${sortedRunTimeData.run_time} in ${sortedRunTimeData.year}`
+        let runCount = getTopByKeyCount(forRace, 'races', 1)[0].races
+        return informationArray = [ runCount, bestStanding, bestFinishTime ]
+    }
 }
 
 export const MusherInformation = (props) => {
